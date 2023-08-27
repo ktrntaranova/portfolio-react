690f9573505ff382f39e8da3b79d6898e41734d9
ktrntaranova <137340777+ktrntaranova@users.noreply.github.com>
2023-08-27 17:42:25 +0300

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import Header from './../components/header/Header'
+
+const Home = () => {
+    return ( 
+        <>
+            <Header />
+            <main className="section">
+                <div className="container">
+
+                    <ul className="content-list">
+                        <li className="content-list__item">
+                            <h2 className="title-2">Frontend</h2>
+                            <p>JavaScript, ReactJS, Redux, HTML, CSS, NPM, BootStrap, TailwindCSS</p>
+                        </li>
+                        <li className="content-list__item">
+                            <h2 className="title-2">Backend</h2>
+                            <p>NodeJS, PHP</p>
+                        </li>
+                    </ul>
+
+                </div>
+            </main>
+        </>
+     );
+}
+ 
+export default Home;
